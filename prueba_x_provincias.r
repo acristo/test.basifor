b82ba31c3cf41dd04351f98e9a043728f16a6bab
acristo <angelcristobal.ordonez@uva.es>
2024-07-02 08:48:53 +0200

    7th commit

---
+++ prueba_x_provincias.r
@@ -0,0 +1,53 @@
+nIFN<-4
+getNFI(provincia=1,nfi = nIFN)[1:10,]# el ifn4 se descarga para el pais vasco
+#getNFI(provincia=2,nfi= nIFN )[1:10,]
+getNFI(provincia=3,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+getNFI(provincia=4,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+getNFI(provincia=5,nfi= nIFN )[1:10,]#enlace mal
+getNFI(provincia=6,nfi= nIFN )[1:10,]# el ifn4 se descarga para extremadura
+#getNFI(provincia=7,nfi= nIFN )[1:10,]
+getNFI(provincia=8,nfi= nIFN )[1:10,]# el ifn4 se descarga para cataluña
+#getNFI(provincia=9,nfi= nIFN )[1:10,]
+getNFI(provincia=10,nfi= nIFN )[1:10,]# el ifn4 se descarga para extremadura
+getNFI(provincia=11,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+getNFI(provincia=12,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+getNFI(provincia='ciud',nfi= nIFN )[1:10,]#con el 13 no funciona
+getNFI(provincia=14,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+getNFI(provincia='lugo',nfi= nIFN )[1:10,]#con el 15 no funciona
+#getNFI(provincia=16,nfi= nIFN )[1:10,]
+getNFI(provincia=17,nfi= nIFN )[1:10,]# el ifn4 se descarga para cataluña
+getNFI(provincia=18,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+#getNFI(provincia=19,nfi= nIFN )[1:10,]
+getNFI(provincia=20,nfi= nIFN )[1:10,]# el ifn4 se descarga para el pais vasco
+getNFI(provincia=21,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+#getNFI(provincia=22,nfi= nIFN )[1:10,]
+#getNFI(provincia=23,nfi= nIFN )[1:10,]
+getNFI(provincia=24,nfi= nIFN )[1:10,]#enlace mal
+getNFI(provincia=25,nfi= nIFN )[1:10,]# el ifn4 se descarga para cataluña
+############# OJO ###############
+getNFI(provincia=26,nfi= nIFN )[1:10,]#-----ESTE DEBERÍA FUNCIONAR-------
+############# OJO ###############
+#getNFI(provincia=27,nfi= nIFN )[1:10,]
+#getNFI(provincia=28,nfi= nIFN )[1:10,]
+getNFI(provincia=29,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+getNFI(provincia=30,nfi= nIFN )[1:10,]#enlace mal
+getNFI(provincia=31,nfi= nIFN )[1:10,]#enlace mal
+#getNFI(provincia=32,nfi= nIFN )[1:10,]
+#getNFI(provincia=33,nfi= nIFN )[1:10,]
+#getNFI(provincia=34,nfi= nIFN )[1:10,]
+getNFI(provincia=35,nfi= nIFN )[1:10,]# el ifn4 se descarga para canarias
+#getNFI(provincia=36,nfi= nIFN )[1:10,]
+#getNFI(provincia=37,nfi= nIFN )[1:10,]
+getNFI(provincia=38,nfi= nIFN )[1:10,]# el ifn4 se descarga para canarias
+getNFI(provincia=39,nfi= nIFN )[1:10,]#enlace mal
+getNFI(provincia=40,nfi= nIFN )[1:10,]#enlace mal
+getNFI(provincia=41,nfi= nIFN )[1:10,]#enlace mal
+#getNFI(provincia=42,nfi= nIFN )[1:10,]
+getNFI(provincia=43,nfi= nIFN )[1:10,]# el ifn4 se descarga para cataluña
+#getNFI(provincia=44,nfi= nIFN )[1:10,]
+#getNFI(provincia=45,nfi= nIFN )[1:10,]
+getNFI(provincia=46,nfi= nIFN )[1:10,]#----------NO ESTÁ AÚN----------
+getNFI(provincia=47,nfi= nIFN )[1:10,]#enlace mal
+getNFI(provincia=48,nfi= nIFN )[1:10,]# el ifn4 se descarga para el pais vasco
+getNFI(provincia=49,nfi= nIFN )[1:10,]#enlace mal
+#getNFI(provincia=50,nfi= nIFN )[1:10,]
